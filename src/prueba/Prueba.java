70fb832d0ed25f62b72229443af38edf0614daa2
“SergioSuaSan” <sergiosuarez95@yahoo.es>
2023-10-31 12:25:04 +0100

    2 commit

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -5,6 +5,11 @@ public class Prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int ganasdevivir = 0;
+		System.out.println(ganasdevivir);
+		
+		
+		
 	}
 
 }
